04a7b0abcff4cc23f5152e7d04062dfe55c4ad5d
Damian Skrzypczak <damian.piotr.skrzypczak@gmail.com>
2020-04-11 02:02:54 +0200

    feat: Add windows support to paths

--- examples/auto_resource/api/schemas/schemas.go
+++ examples/auto_resource/api/schemas/schemas.go
@@ -2,7 +2,9 @@
 package schemas
 
 import (
+	"net/http"
 	"runtime"
+	"strings"
 
 	"github.com/rakyll/statik/fs"
 	"github.com/xeipuuv/gojsonschema"
@@ -19,18 +21,35 @@ func MustLoadMovieSchema(name string) gojsonschema.JSONLoader {
 	return schema
 }
 
+const win = "windows"
+
+// FSWrapper adapts statik file system (supporting unix-like paths) to windows paths
+// by replacing "c:\" prefix with "/"
+//
+
+type FSWrapper struct { // Remove if you don't need windows support
+	fs http.FileSystem
+}
+
+func (fsw FSWrapper) Open(name string) (http.File, error) { // Remove if you don't need windows support
+	if runtime.GOOS == win {
+		name = strings.Replace(name, "c:\\", "/", 1)
+	}
+
+	return fsw.fs.Open(name)
+}
+
 func loadSchema(name, namespace string) (gojsonschema.JSONLoader, error) {
 	statikFS, err := fs.NewWithNamespace(namespace)
 	if err != nil {
 		return nil, err
 	}
 
-	var path string
-	if runtime.GOOS == "windows" {
-		path = "file://d:/" + name
-	} else {
-		path = "file:///" + name
+	path := "file:///" + name
+
+	if runtime.GOOS == win { // Remove if you don't need windows support
+		path = "file://c:/" + name
 	}
 
-	return gojsonschema.NewReferenceLoaderFileSystem(path, statikFS), nil
+	return gojsonschema.NewReferenceLoaderFileSystem(path, FSWrapper{statikFS}), nil
 }
